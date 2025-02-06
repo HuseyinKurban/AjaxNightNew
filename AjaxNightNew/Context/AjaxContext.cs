66a7811c585e15e2d5f6a69c5d876baf68677922
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-02-06 21:54:11 +0300

    Add project files.

---
+++ AjaxNightNew/Context/AjaxContext.cs
@@ -0,0 +1,15 @@
+﻿using AjaxNightNew.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AjaxNightNew.Context
+{
+    public class AjaxContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-32Q9FH5;initial catalog=AjaxNightDb;integrated security=true;TrustServerCertificate=true");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ AjaxNightNew/Entities/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace AjaxNightNew.Entities
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerSurname { get; set; }
+    }
+}
